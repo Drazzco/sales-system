4b789133663575b96b69c644673e848bda42fad9
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2023-07-18 15:59:21 -0300

    Creating Class Models

---
+++ src/main/java/io/github/drazzco/domain/entities/Order.java
@@ -0,0 +1,40 @@
+package io.github.drazzco.domain.entities;
+
+import java.time.LocalDate;
+
+public class Order {
+
+	private Integer id;
+	private Customer customer;
+	private LocalDate date;
+	private Integer total;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public Integer getTotal() {
+		return total;
+	}
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/io/github/drazzco/domain/entities/Product.java
@@ -0,0 +1,30 @@
+package io.github.drazzco.domain.entities;
+
+public class Product {
+
+	private Integer id;
+	private String description;
+	private Double price;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+	
+}
